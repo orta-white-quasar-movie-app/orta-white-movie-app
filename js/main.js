504fe78e6b7f6c27ae3976219f43d4a8abc019dc
Mary White <mary.a.white28@gmail.com>
2021-10-15 17:32:55 -0400

    added bootstrap cdn and jquery script, linked html to js and css sources

--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
 "use strict"
+
+
+//*** Notes from Fridays lecture ***
+//const API_URL = "https://vagabond-glacier-verse.glitch.me/dogs";
+
+// function getDogs(){
+//     return fetch(API_URL)
+//         .then((response) => response.json());
+// }
+//
+// function getDog(id){
+//     return fetch(`${API_URL}/${id}`)
+//         .then((response)=> response.json())
+// }
+//in the console, the user would type getDog(11).then((dog)=>console.log(dog))
+
+//Edit dog by ID
+// function editDog(dog) {
+//     let options = {
+//         method: 'PUT',       //use put to edit the dog, we are not creating a new one
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(dog)    //what is body. body is referring to the body of the request
+//     }
+//     return fetch(`${API_URL}/${dog.id}`, options)
+//         .then((response)=>response.json())
+// }
+// let piper = {
+//     name: 'Piper',
+//     isGoodDog: false,
+//     id: 11,
+//     age: 8
+// }
+
+//editDog(piper).then((data)=>console.log(data))
+
+// function deleteDog(id){
+//     let options = {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     }
+//     fetch(`${API_URL}/${id}, options`)
+//         .then((response) => console.log("deleted dog"))
+// }
+
+//Create dog
+// function createDog(dog) {
+//     let options = {
+//         method: 'POST',       //use put to edit the dog, we are not creating a new one
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(dog)
+//     }
+//     return fetch(API_URL, options)
+//         .then((response)=>response.json())
+// }
+// let charlie = {
+//     name: 'Charlie',
+//     isGoodDog: false,
+//     age: 8
+// }
+//createDog(charlie).then((newDog)=>console.log(newdog));
